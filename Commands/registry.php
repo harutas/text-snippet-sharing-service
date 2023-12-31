7d3ce3607c60417f458c6b132aefb1ccc7d1ffd8
harutas <96802323+harutas@users.noreply.github.com>
2023-12-31 22:36:21 +0900

    fix: delete registry commands

--- Commands/registry.php
+++ Commands/registry.php
@@ -1,7 +1,5 @@
 <?php return array(
   0 => 'Commands\\Programs\\Migrate',
   1 => 'Commands\\Programs\\CodeGeneration',
-  2 => 'Commands\\Programs\\DBWipe',
-  3 => 'Commands\\Programs\\BookSearch',
-  4 => 'Commands\\Programs\\Seed',
+  2 => 'Commands\\Programs\\DBWipe'
 );
